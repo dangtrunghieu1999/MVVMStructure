9881d5408614e37b1d71e2370daaa909abfdb57b
Dang Trung Hieu <hieudt230899@gmail.com>
2020-11-01 15:32:20 +0700

    create source

---
+++ MVVMStruture/MVVMStruture/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  MVVMStruture
+//
+//  Created by Dang Trung Hieu on 11/1/20.
+//  Copyright © 2020 Dang Trung Hieu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor.white
+        navigationItem.title = "HomeView"
+    }
+
+
+}
+
