76c53df1bc7582920c423e541d995d92c84fbcf6
Dang Trung Hieu <hieudt230899@gmail.com>
2020-11-03 17:18:21 +0700

    complete structure MVVM

--- MVVMStruture/MVVMStruture/Scenes/PictureList/View/PictureListViewController.swift
+++ MVVMStruture/MVVMStruture/Scenes/PictureList/View/PictureListViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class PictureListViewController: UIViewController {
-
+    
     // MARK: - UI Elements
     
     @IBOutlet weak var tableView: UITableView!
@@ -28,18 +28,65 @@ class PictureListViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        adapter = PictureListAdapter(delegate: self)
+        tableView.delegate = adapter
+        tableView.dataSource = adapter
+        
         setupUI()
+        initViewModel()
     }
     
     func setupUI() {
         self.navigationItem.title = "Popular"
     }
     
+    func initViewModel() {
+        viewModel.reloadTableViewClosure =  { [weak self] () in
+            DispatchQueue.main.async {
+                self?.tableView.reloadData()
+            }
+        }
+        
+        viewModel.updateLoadingStatusClousure = { [weak self] () in
+            DispatchQueue.main.async {
+                let isLoading = self?.viewModel.isLoading ?? false
+                if isLoading {
+                    self?.activityIndicator.startAnimating()
+                    UIView.animate(withDuration: 0.2, animations: {
+                        self?.tableView.alpha = 0.0
+                    })
+                } else {
+                    self?.activityIndicator.stopAnimating()
+                    UIView.animate(withDuration: 0.2, animations: {
+                        self?.tableView.alpha = 1.0
+                    })
+                }
+            }
+        }
+        
+        viewModel.fetchData()
+    }
     
     
-    
-    
-    
-    
+}
 
+extension PictureListViewController: PictureListProtocol {
+    func getData(at: IndexPath) -> Picture {
+        return self.viewModel.getData(at: at)
+    }
+    
+    func itemSelected(at: IndexPath) {
+        self.viewModel.selectedPicture = self.viewModel.getData(at: at)
+        performSegue(withIdentifier: "goToDetails", sender: nil)
+    }
+    
+    func retrieveNumberOfSections() -> Int {
+        return 1
+    }
+    
+    func retrieveNumberOfItems() -> Int {
+        return self.viewModel.numberOfItems
+    }
+    
+    
 }
